702a7cf1ced27614d822534e30247c4c2d56ce3d
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-01-30 03:06:08 -0600

    up to date

--- lib/unit.rb
+++ lib/unit.rb
@@ -1,10 +1,24 @@
-class Unit_Type
+require "pry"
+
+# TEST_UNIT_OPTIONS = {
+#   name: "meter",
+#   sym: "m",
+#   multiplier: 1.0,
+#   system: "metric",
+# }
+
+class Unit
   attr_reader :unit
-  def initialize(unit)
-    @unit = unit
-  end
 
-  def 
+  def initialize(name, sym, multiplier)
+    multiplier = multiplier.to_f # throw error if multiplier.class is not a "float"
+    # system = options[:system] || "other"
 
+    @name = name
+    @sym = sym
+    @multiplier = multiplier
+    # @system = system
   end
 end
+
+binding.pry
