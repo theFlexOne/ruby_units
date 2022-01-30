aaba2c6e94554a041c9afe26ebab05d5653f71e5
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-01-29 22:21:41 -0600

    init

---
+++ lib/unit.rb
@@ -0,0 +1,10 @@
+class Unit_Type
+  attr_reader :unit
+  def initialize(unit)
+    @unit = unit
+  end
+
+  def 
+
+  end
+end
---
+++ spec/measurement_spec.rb
@@ -0,0 +1,9 @@
+require "pry"
+require_relative "../lib/measurement.rb"
+
+# describe "Measurement" do
+#   test_measurement = Measurement.new(1, "")
+#   it "is instantiated with 'value' & 'unit' properties" do
+#     expect(test_meas.value).to eq(1)
+#   end
+# end
